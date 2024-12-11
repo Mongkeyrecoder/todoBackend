f297adfa9ec515815cc71670ede4310f10f23b6b
lee jongwoon <roykean1993@gmail.com>
2024-12-11 19:48:38 +0900

    second

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const indexRouter=require('./routes/index')
+const app= express()
+const cors = require('cors')
+require('dotenv').config()
+ const MONGODB_URI_PROD=process.env.MONGODB_URI_PROD;
+ const mongoURI='mongodb+srv://admin:seoul2022@cluster.idqly55.mongodb.net/todo-student'
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use('/api',indexRouter)
+
+
+
+mongoose.connect(mongoURI).then(()=>console.log('mongoose connected'))
+.catch((err)=>{console.log('db connection fail',err)})
+
+app.listen(5000,()=>{
+    console.log('server on 5000')
+})
